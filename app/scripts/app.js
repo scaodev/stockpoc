5417e0452cca433d3043ee0b7e05229f82bcc6e0
SHENGJIE CAO <scaodev@gmail.com>
2013-03-08 15:52:22 -0500

    init project with yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var stockpocApp = angular.module('stockpocApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
